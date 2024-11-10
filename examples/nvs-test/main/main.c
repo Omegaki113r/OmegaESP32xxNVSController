f463fa9a4ba531c33bb6036dbdc3db15f6d313c7
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-11-10 22:09:43 +0530

    nvs example added

---
+++ examples/nvs-test/main/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#include <esp_err.h>
+
+#include "OmegaESP32xxNVSController.h"
+#include "OmegaUtilityDriver.h"
+
+void app_main(void)
+{
+    if (eSUCCESS != OmegaESP32xxNVSController_init(NULL))
+        OMEGA_LOGE("NVS Controller initialization failed");
+    int write = 0;
+    int read = 0;
+    float writef = 0;
+    float readf = 0;
+    for (;;)
+    {
+        write += 1;
+        writef += 1.0f;
+        OmegaESP32xxNVSController_write_int("data", write);
+        OmegaESP32xxNVSController_read_int("data", &read);
+        OMEGA_LOGI("Write Value: %.2d Read value: %.2d", write, read);
+        OmegaESP32xxNVSController_write_float("dataf", writef);
+        OmegaESP32xxNVSController_read_float("dataf", &readf);
+        OMEGA_LOGI("Write Value: %.2f Read value: %.2f", writef, readf);
+        delay_ms(100);
+    }
+}
